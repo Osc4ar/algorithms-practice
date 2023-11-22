b84c0c61e01b10c46e391f2d8ddbb2424dfcdec9
Oscar Castillo <oscar.castillo.magana@gmail.com>
2023-11-22 08:31:03 -0600

    LC133: Clone Graph with BFS

---
+++ leetcode/133_CloneGraph.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return None
+
+        cloned_first_node = Node(val=node.val)
+        cloned_nodes = {
+            1: cloned_first_node
+        }
+
+        next_nodes = deque()
+        next_nodes.append(node)
+
+        while next_nodes:
+            original_node = next_nodes.popleft()
+            cloned_node = cloned_nodes[original_node.val]
+
+            for neighbor in original_node.neighbors:
+                if neighbor.val not in cloned_nodes:
+                    cloned_nodes[neighbor.val] = Node(val=neighbor.val)
+                    next_nodes.append(neighbor)
+
+                cloned_neighbor = cloned_nodes[neighbor.val]
+                cloned_node.neighbors.append(cloned_neighbor)
+
+        return cloned_first_node
