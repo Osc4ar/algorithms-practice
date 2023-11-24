960836dd1d7c7793dc09206b575f610ef3a8e760
Oscar Castillo <oscar.castillo.magana@gmail.com>
2023-11-24 00:57:31 -0600

    LC322: BFS approach

--- leetcode/322_CoinChange.py
+++ leetcode/322_CoinChange.py
@@ -1,20 +1,29 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        return self.memoization(coins, amount, {})
-
-    def memoization(self, coins: List[int], amount: int, memory: dict[int, int]) -> int:
         if amount == 0:
             return 0
 
-        if amount in memory:
-            return memory[amount]
+        possible_paths = deque()
+        visited_amounts = set()
+        possible_paths.append(amount)
+        visited_amounts.add(amount)
 
-        options = []
-        for c in coins:
-            options.append(self.memoization(coins, amount - c, memory))
+        numbers_of_coins = 0
+        while possible_paths:
+            numbers_of_coins += 1
+            for _ in range(len(possible_paths)):
+                current_amount = possible_paths.popleft()
 
-        memory[amount] = 1 + min(options)
-        return memory[amount]
+                for c in coins:
+                    next_amount = current_amount - c
+                    if next_amount == 0:
+                        return numbers_of_coins
+                    if next_amount not in visited_amounts and next_amount > 0:
+                        possible_paths.append(next_amount)
+                        visited_amounts.add(next_amount)
+
+        return -1
+
 
 '''
 coinChange([1,2,5], 11)
@@ -26,8 +35,11 @@ min(coinChange([1,2,5], 9), coinChange([1,2,5], 7), coinChange([1,2,5], 5))
 coinChange([1,2,5], 5)
 min(coinChange([1,2,5], 4), coinChange([1,2,5], 3), coinChange([1,2,5], 0))
 
-11
-[10, 9, 6]
+11 [1,2,5]
+[10, 9, 6] -> [9, 8, 5, 8, 7, 4, 5, 4, 1] -> (9, 8, 5, 4, 1) -> (8, 7, 6, 4, 3, 0)
+[5, 4, 1]
+[0, 2, 0]
 
+32 [1,2,5]
 []
 '''
