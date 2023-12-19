0e99bfb5738659c98c89e83108cb1d7289da6a00
Oscar Castillo <oscar.castillo.magana@gmail.com>
2023-12-19 08:50:20 -0600

    LC63: DP with in-place memory

--- leetcode/63_uniquePathsII.py
+++ leetcode/63_uniquePathsII.py
@@ -5,17 +5,12 @@ class Solution:
 
         previous_row = [0] * columns
         previous_row[columns - 1] = 1
+
         for row in reversed(range(rows)):
-            current_row = [0] * columns
-            
             for column in reversed(range(columns)):
                 if obstacleGrid[row][column] == 1:
-                    current_row[column] = 0
+                    previous_row[column] = 0
                 elif column + 1 < columns:
-                    current_row[column] = current_row[column + 1] + previous_row[column]
-                else:
-                    current_row[column] = previous_row[column]
-
-            previous_row = current_row
+                    previous_row[column] = previous_row[column + 1] + previous_row[column]
 
         return previous_row[0]
