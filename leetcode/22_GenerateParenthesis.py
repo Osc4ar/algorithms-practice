a7f2cd73d0ca7c096fc17518a0b88b1749de613e
Oscar Castillo <oscar.castillo.magana@gmail.com>
2024-02-20 22:02:11 -0600

    LC22: Backtracking using list

--- leetcode/22_GenerateParenthesis.py
+++ leetcode/22_GenerateParenthesis.py
@@ -1,20 +1,20 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        self.generator('', 0, 0, n, result)
+        self.generator([], 0, 0, n, result)
         return result
 
 
-    def generator(self, current: str, opened: int, closed: int, n: int, result: List[str]):
+    def generator(self, current: List[str], opened: int, closed: int, n: int, result: List[str]):
         if opened == n and closed == n:
-            result.append(current)
+            result.append(''.join(current))
             return
 
         if opened < n:
-            current += '('
+            current.append('(')
             self.generator(current, opened + 1, closed, n, result)
-            current = current[:-1]
+            current.pop()
         if opened > closed:
-            current += ')'
+            current.append(')')
             self.generator(current, opened, closed + 1, n, result)
-            current = current[:-1]
+            current.pop()
