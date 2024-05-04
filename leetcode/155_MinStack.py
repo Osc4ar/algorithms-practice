7ff77b94f95760d37bd4ab6632f0a3253c525629
Oscar Castillo <oscar_97.10@outlook.com>
2024-05-03 21:17:01 -0600

    MinStack with a two pair value

---
+++ leetcode/155_MinStack.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        min_val = val
+        if len(self.stack):
+            last_val = self.stack[-1]
+            min_val = min(min_val, last_val[1])
+
+        self.stack.append((val, min_val))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
