40b41fed8be1e392beffe60c3c90c96a466b0d11
Oscar Castillo <oscar_97.10@outlook.com>
2025-01-09 22:42:45 -0600

    LC70: Using DP

--- leetcode/70_climbingStairs.py
+++ leetcode/70_climbingStairs.py
@@ -1,13 +1,18 @@
-def climbStairs(n: int, current: int) -> int:
-    if current == n:
-        return 1
-    
-    if current > n:
-        return 0
-    
-    if current is None:
-        current = 0
-    
-    return climbStairs(n, current+1) + climbStairs(n, current+2)
+class Solution:
+    '''
+    c(n) = c(n-1) + c(n-2)
+    '''
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
 
-print(climbStairs(6, 0))
+        prev = 2
+        before_prev = 1
+        for i in range(2, n):
+            tmp = prev
+            prev += before_prev
+            before_prev = tmp
+
+        return prev
