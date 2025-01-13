17449dfd3a52e82c6d7b0aae17fa37e73ba04eb3
Oscar Castillo <oscar_97.10@outlook.com>
2025-01-12 22:53:22 -0600

    LC15: Using sorted array

--- leetcode/15_3sum.py
+++ leetcode/15_3sum.py
@@ -1,25 +1,42 @@
 class Solution:
+    '''
+    1. Sort the array to make the search faster with two pointers [-4, -1, -1 0, 1, 2]
+    2. Take the first value x and focus in solve two sum
+    3. Since the array is sorted we can have two pointers, one in the beginning of the search
+    and another at the end.
+    4. If the number is smaller than the target, we move the left pointer
+    5. If the number is greater than the target, we move the right pointer
+    6. If we find the target we save the result and keep looking moving the left pointer
+
+          x     *  *
+    [-4, -1, -1 0, 1, 2]
+    target = 1
+    '''
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        nums.sort() # O(nlogn)
 
         result = []
+        visited = set()
+        # O(n^2)
         for i, n in enumerate(nums):
-            if i > 0 and n == nums[i - 1]:
+            if n in visited:
                 continue
 
+            target = -1*n
             left = i + 1
             right = len(nums) - 1
-
             while left < right:
-                current_sum = n + nums[left] + nums[right]
-                if current_sum > 0:
-                    right -= 1
-                elif current_sum < 0:
+                current = nums[left] + nums[right]
+                if current == target:
+                    result.append([nums[i], nums[left], nums[right]])
+                    prev = nums[left]
+                    while left < len(nums) and prev == nums[left]:
+                        left += 1
+                elif current < target:
                     left += 1
                 else:
-                    result.append([n, nums[left], nums[right]])
-                    old_value = nums[left]
-                    while left < len(nums) and old_value == nums[left]:
-                        left += 1
+                    right -= 1
+
+            visited.add(n)
 
         return result
