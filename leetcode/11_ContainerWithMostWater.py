2330cc87b1227dd095dba9f3275e3dc08b408a2e
Oscar Castillo <oscar_97.10@outlook.com>
2025-01-15 22:45:49 -0600

    LC11: Sliding window with explanation

--- leetcode/11_ContainerWithMostWater.py
+++ leetcode/11_ContainerWithMostWater.py
@@ -1,14 +1,17 @@
 class Solution:
+    '''
+    1. We can start with a container with the biggest distance from start to finish
+    2. To maximize the size of the container, we can move the pointer of the smallest line
+    3. We save the max container and keep iterating until the pointers meet at the middle
+    4. We return the max container we found
+    '''
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = len(height) - 1
         max_area = 0
-
         while left < right:
-            min_height = min(height[left], height[right])
-            current_area = min_height * (right - left)
-            max_area = max(max_area, current_area)
-
+            area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, area)
             if height[left] < height[right]:
                 left += 1
             else:
