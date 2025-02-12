2c86cd2bdffdd9ed96e30678508e09007f60dd0d
Oscar Castillo <oscar_97.10@outlook.com>
2025-02-12 08:26:18 -0600

    LC88: Using a single loop and catching edge cases inside it

--- leetcode/88_MergeTwoSortedArrays.py
+++ leetcode/88_MergeTwoSortedArrays.py
@@ -1,19 +1,20 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        pointer1 = m - 1
-        pointer2 = n - 1
-        mergePointer = m + n - 1
-        
-        while pointer1 >= 0 and pointer2 >= 0:
-            if nums1[pointer1] >= nums2[pointer2]:
-                nums1[mergePointer] = nums1[pointer1]
-                pointer1 -= 1
+        i = m - 1
+        j = n - 1
+        merge = m + n - 1
+
+        while i >= 0 or j >= 0:
+            if i < 0 and j >= 0:
+                nums1[merge] = nums2[j]
+                j -= 1
+            elif j < 0 and i >= 0:
+                nums1[merge] = nums1[i]
+                i -= 1
+            elif nums1[i] > nums2[j]:
+                nums1[merge] = nums1[i]
+                i -= 1
             else:
-                nums1[mergePointer] = nums2[pointer2]
-                pointer2 -= 1
-            mergePointer -= 1
-        
-        while pointer2 >= 0 and mergePointer >= 0:
-            nums1[mergePointer] = nums2[pointer2]
-            pointer2 -= 1
-            mergePointer -= 1
+                nums1[merge] = nums2[j]
+                j -= 1
+            merge -= 1
