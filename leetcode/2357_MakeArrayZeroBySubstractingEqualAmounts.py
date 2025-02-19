364f7c3182e56d0581d6027042c72dec7ab53538
Oscar Castillo <oscar_97.10@outlook.com>
2025-02-19 09:09:39 -0600

    LC2357: Using a set to count unique non-zero values

--- leetcode/2357_MakeArrayZeroBySubstractingEqualAmounts.py
+++ leetcode/2357_MakeArrayZeroBySubstractingEqualAmounts.py
@@ -1,30 +1,14 @@
 class Solution:
     '''
-    Brute Force solution O(n^2)
-    Get the minimum of the current array, substract it from the array
-    Repeat the operation until all elements of the array are zero or the array is empty if you skip zero values
+    We can say that every non-zero value is a step to reach a list full of zeroes
+    It does not matter if we have duplicate values because we can do the operation to all values
 
-
-    Optimal solution O(n)
-    1. Get the max from the array
-    2. Remove any duplicate
-    3. Add all the values on a min heap to get the smallest value in constant time
-    4. Keep a sum of the min values, every time you pop a value add to it the difference between it and the current sum
-    5. Repeat until the sum is greater than or equal to the max value 
+    Therefore we only need to count the number of unique non-zero values, for that we can use a set 
     '''
     def minimumOperations(self, nums: List[int]) -> int:
-        target = max(nums)
-        no_duplicates = list(set(nums))
-        heapq.heapify(no_duplicates) # Linear time complexity
+        uniques = set(nums)
 
-        if no_duplicates[0] == 0:
-            heapq.heappop(no_duplicates)
+        if 0 in uniques:
+            uniques.remove(0)
 
-        current = 0
-        count = 0
-        while current < target:
-            step = heapq.heappop(no_duplicates) - current
-            current += step
-            count += 1
-
-        return count
+        return len(uniques)
