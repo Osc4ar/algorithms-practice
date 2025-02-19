161023d75c45ebc2acf52bcdfe17fdc68fd6ea00
Oscar Castillo <oscar_97.10@outlook.com>
2025-02-19 09:05:51 -0600

    LC49: Using letter frequencies as key O(n*m)

--- leetcode/49_GroupAnagrams.py
+++ leetcode/49_GroupAnagrams.py
@@ -3,7 +3,7 @@ class Solution:
     We can create a dictionary where the key is a sorted string and the value is a list of anagrams 
 
     1. For every string we do:
-        1. Sort the string, that will be the key
+        1. Get the frequency of each letter, use it as the key
         2. If the key already exists, append the string to that list
         3. If it does not exist, add it to the dictionary with a new list with the string as its single value
     2. Return a list with all the lists saved on the dictionary
@@ -12,7 +12,11 @@ class Solution:
         anagrams = defaultdict(list)
 
         for s in strs:
-            key = ''.join(sorted(s))
+            freq = [0] * 26
+            for c in s:
+                freq[ord(c) - ord('a')] += 1
+            key = tuple(freq)
+
             anagrams[key].append(s)
 
-        return [group for group in anagrams.values()]
+        return list(anagrams.values())
