013b5127a2daa350b7c405830dd233cdcaa67a9e
Oscar Castillo <oscar_97.10@outlook.com>
2025-02-23 09:48:05 -0600

    LC1152: Optimization using combinations function

--- leetcode/1152_AnalyzeUserWebsiteVisitPattern.py
+++ leetcode/1152_AnalyzeUserWebsiteVisitPattern.py
@@ -1,26 +1,4 @@
 class Solution:
-    '''
-    We can read the arrays and build a list of the websites visited by each user:
-
-    {
-        'joe': ['home', 'about', 'career'],
-        'james': ['home', 'cart', 'maps', 'home'],
-        'mary': ['home', 'about', 'career']
-    }
-
-    We can discard any user with less than 3 websites visited. For the others:
-    1. Make tuples of tree elements with the websites visited
-    2. Check any combination of them, register them in a score dictionary where the key is the tuple
-    and the value the scores
-    3. Get the tuple with the highest score, if there are more than one return the lexicographically smallest
-
-        *       *              *
-    ['home', 'cart', 'maps', 'home']
-    [0, 1, 2]
-    [0, 1, 3]
-    [0, 2, 3]
-    [1, 2, 3]
-    '''
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         time_websites_by_user = defaultdict(list)
         count_by_user = defaultdict(int)
@@ -38,15 +16,10 @@ class Solution:
                 websites_by_user[user].append(website)
 
         tuples_by_user = defaultdict(set)
-        tuples_by_score = defaultdict(int)
+        tuples_by_score = Counter()
         for user, websites in websites_by_user.items():
-            for i in range(len(websites) - 2):
-                for j in range(i + 1, len(websites) - 1):
-                    for k in range(j + 1, len(websites)):
-                        t = (websites[i], websites[j], websites[k])
-                        if t not in tuples_by_user[user]:
-                            tuples_by_user[user].add(t)
-                            tuples_by_score[t] += 1
+            combs = set(combinations(websites, 3))
+            tuples_by_score.update(combs)
 
         max_tuples = []
         max_score = float('-inf')
