d4e6fe98ef4c7c3f73a9bb4dee7f2972f455cfe6
Oscar Castillo <oscar_97.10@outlook.com>
2025-03-16 09:31:40 -0600

    LC210: Using DFS and a set to avoid duplicates in the path

--- leetcode/210_CourseScheduleII.py
+++ leetcode/210_CourseScheduleII.py
@@ -1,42 +1,42 @@
 class Solution:
     '''
-    1: {0},
-    2: {0},
-    3: {1, 2}
-
-    1. Create an adjacency list of the course and its required courses. Also save a set with the courses without
-       prerequesites.
-    2. We can save a set of "taken" courses, the initial values are the courses with no prerequesites.
-    3. While the taken courses are less than the numCourses check which courses we can take.
-    4. If we can take a course, we add the course to the taken set and remove it from the HashMap
-    5. If we did not added a new course, it means we cannot complete the courses
+    1. Build an adjacency list with the course and its prereqs
+    2. Using DFS, check if we can finish the course, if we can add it to the result
+    3. If we find a cycle and it is not possible to finish the courses, return False from DFS
+    4. If we finish all courses, return the list with the order the courses were visited
     '''
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prequisites_by_course = defaultdict(set)
-        taken_courses = set(range(numCourses))
+        adj = defaultdict(list)
+        for course, prereq in prerequisites:
+            adj[course].append(prereq)
 
-        for course, prerequisite in prerequisites:
-            prequisites_by_course[course].add(prerequisite)
-            taken_courses.discard(course)
-
-        last_size = None
-        current_size = len(taken_courses)
-        result = list(taken_courses)
-        while current_size < numCourses:
-            if last_size == current_size:
-                return []
-
-            courses_to_remove = set()
-            for course, prerequisite in prequisites_by_course.items():
-                if prerequisite.issubset(taken_courses):
+        result = []
+        visited = set()
+        def dfs(course: int, path: set) -> bool:
+            prereq = adj[course]
+            if len(prereq) == 0:
+                if course not in visited:
+                    visited.add(course)
                     result.append(course)
-                    taken_courses.add(course)
-                    courses_to_remove.add(course)
+                return True
 
-            for course in courses_to_remove:
-                prequisites_by_course.pop(course)
+            for next_course in prereq:
+                if next_course in path:
+                    return False
 
-            last_size = current_size
-            current_size = len(taken_courses)
+                path.add(next_course)
+                if not dfs(next_course, path):
+                    return False
+                path.remove(next_course)
 
+            adj[course] = []
+            result.append(course)
+            visited.add(course)
+            return True
+            
+
+        for course in range(numCourses):
+            if not dfs(course, set()):
+                return []
+        
         return result
