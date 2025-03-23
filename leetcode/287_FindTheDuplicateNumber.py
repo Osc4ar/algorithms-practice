bc06a28b956c504a2d235a4cfa27c205866fa4d9
Oscar Castillo <oscar_97.10@outlook.com>
2025-03-23 09:33:02 -0600

    LC287: Using constant memory by marking visited nodes in array

--- leetcode/287_FindTheDuplicateNumber.py
+++ leetcode/287_FindTheDuplicateNumber.py
@@ -1,8 +1,14 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        a_set = set()
+        result = 0
+        for i in range(len(nums)):
+            if nums[abs(nums[i]) - 1] < 0:
+                result = abs(nums[i])
+                break
+            
+            nums[abs(nums[i]) - 1] *= -1
 
-        for n in nums:
-            if n in a_set:
-                return n
-            a_set.add(n)
+        for i in range(len(nums)):
+            nums[i] = abs(nums[i])
+
+        return result
