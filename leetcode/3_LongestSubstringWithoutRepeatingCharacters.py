29722c8589bf5aa0e74f6f44bc7d722cd3fdb5c5
Oscar Castillo <oscar_97.10@outlook.com>
2025-03-24 08:50:59 -0600

    LC3: Optimized conditions using a single while for left pointer

--- leetcode/3_LongestSubstringWithoutRepeatingCharacters.py
+++ leetcode/3_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,16 +1,21 @@
 class Solution:
+    '''
+    1. Use sliding window technique, both pointers will start at 0
+    2. Keep a set to know if there is duplicates
+    3. Expand right pointer one position, if the string is on the set reduce the window until the window is valid again
+    4. Once we have a valid window, we save the max window size as the result
+    5. After iterating the whole array return the max window size
+    '''
     def lengthOfLongestSubstring(self, s: str) -> int:
-        substring = set()
-        longest = 0
+        result = 0
+        chars = set()
         left = 0
-        
         for right in range(len(s)):
-            if s[right] in substring:
-                longest = max(longest, len(substring))
-                while s[right] in substring:
-                    substring.remove(s[left])
-                    left += 1
+            while left < right and s[right] in chars:
+                chars.remove(s[left])
+                left += 1
+            
+            chars.add(s[right])
+            result = max(result, len(chars))
 
-            substring.add(s[right])
-
-        return max(longest, len(substring))
+        return result
