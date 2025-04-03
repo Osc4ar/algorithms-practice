1a4f915d1d34bf178882115905a0ee229b86cc4d
Oscar Castillo <oscar_97.10@outlook.com>
2025-04-03 08:04:29 -0600

    LC424: Optimized code using a HashMap

--- leetcode/424_LongestRepeatingCharacterReplacement.py
+++ leetcode/424_LongestRepeatingCharacterReplacement.py
@@ -1,36 +1,33 @@
 class Solution:
+    '''
+      L  R
+    AABABBA
+
+    1. Store the frequency of each letter on a HashMap
+    2. Any time we expand our window, add one to the letter
+    3. Find the most common letter, check if its Window Size - Frequency is less than or equal than k
+    4. If it is, we have a valid window, save it if it is bigger than the previous one
+    5. If it is not, reduce the window size.
+    6. While reducing the window size, check if the most common letter changes.
+    7. Reduce the window until it is valid again
+    8. Return the max window calculated after iterating the whole array
+    '''
     def characterReplacement(self, s: str, k: int) -> int:
-        if len(s) == 1:
-            return 1
-
-        result = 1
-        frequencies = {}
+        result = 0
         left = 0
+        freqs = defaultdict(int)
+
         for right in range(len(s)):
-            if s[right] not in frequencies:
-                frequencies[s[right]] = 1
-            else:
-                frequencies[s[right]] += 1
-
-            max_frequency = self.getMaxFrequency(frequencies)
+            freqs[s[right]] += 1
             window_size = right - left + 1
-            is_valid_str = window_size - max_frequency <= k
+            most_frequent = max(freqs.values()) # O(26)
 
-            if is_valid_str:
-                result = max(result, window_size)
-            else:
-                while not is_valid_str:
-                    frequencies[s[left]] -= 1
-                    left += 1
-                    window_size = right - left + 1
-                    is_valid_str = window_size - max_frequency <= k
-            
+            while left <= right and window_size - most_frequent > k:
+                freqs[s[left]] -= 1
+                left += 1
+                most_frequent = max(freqs.values()) # O(26)
+                window_size = right - left + 1
+
+            result = max(result, window_size)
+
         return result
-
-    def getMaxFrequency(self, frequencies: [str, int]) -> int:
-        max_frequency = 0
-
-        for frequency in frequencies.values():
-            max_frequency = max(max_frequency, frequency)
-
-        return max_frequency
