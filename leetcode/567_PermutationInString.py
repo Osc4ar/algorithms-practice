9f625b9cee7217cf8bd76bdc75c111d3173ea29d
Oscar Castillo <oscar_97.10@outlook.com>
2025-04-11 07:52:47 -0600

    LC567: Using for loop to move the window

--- leetcode/567_PermutationInString.py
+++ leetcode/567_PermutationInString.py
@@ -1,28 +1,20 @@
 class Solution:
-    '''
-    We can keep a dictionary with the frequency of each letter on s1
-    We have a window of the size of s1 to review the characters on s2
-    We keep moving the window and verifying against the frequencies of s1 to see
-    if s2 has a permutation of s1
-    '''
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_frequencies = Counter(s1)
+        freqs_s1 = Counter(s1)
+        freqs_s2 = Counter(s2[:len(s1)])
+
+        if freqs_s1 == freqs_s2:
+            return True
 
         left = 0
-        right = len(s1) - 1
+        right = len(s1)
+        for right in range(len(s1), len(s2)):
+            freqs_s2[s2[right]] += 1
+            freqs_s2[s2[left]] -= 1
 
-        s2_window = s2[left:right+1]
-        s2_frequencies = Counter(s2_window)
-
-        while right < len(s2):
-            if s2_frequencies == s1_frequencies:
+            if freqs_s1 == freqs_s2:
                 return True
 
-            s2_frequencies[s2[left]] -= 1
-            if right + 1 < len(s2):
-                s2_frequencies[s2[right + 1]] += 1
-
-            right += 1
-            left += 1 
+            left += 1
 
         return False
